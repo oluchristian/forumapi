1201e0c899c37355d347aa2de8523a5f9a5ba2f9
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-06-04 15:32:59 +0100

    new commit

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function(){
+    return response([
+        'message' => 'api is working',
+    ], 200);
+});
+
+Route::post('/register', [AuthenticationController::class, 'register']);
